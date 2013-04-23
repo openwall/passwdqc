9963845ff3195470bfc9f7a7753b06cf20c1df09
Solar Designer <solar@openwall.com>
2013-04-23 16:16:18 +0400

    Added cpp macros for the constants, added source code comments.  This source code change does not affect the generated code.
    CVS-ID: passwdqc_random.c 1.16

--- passwdqc_random.c
+++ passwdqc_random.c
@@ -12,6 +12,11 @@
 #include "wordset_4k.h"
 
 /*
+ * Maximum word length found in _passwdqc_wordset_4k[].
+ */
+#define WORD_LENGTH_MAX			6
+
+/*
  * We separate words in the generated "passphrases" with random special
  * characters out of a set of 16 (so we encode 4 bits per separator
  * character).  To enable the use of our "passphrases" within FTP URLs
@@ -24,6 +29,37 @@
  * might be confused for the digit 1 (which we don't use), though.
  */
 #define SEPARATORS			"-_!$&*+=23456789"
+
+/*
+ * Number of bits encoded per separator character.
+ */
+#define SEPARATOR_BITS			4
+
+/*
+ * Number of bits encoded per word.  We use 4096 words, which gives 12 bits,
+ * and we toggle the case of the first character, which gives one bit more.
+ */
+#define WORD_BITS			13
+
+/*
+ * Number of bits encoded per separator and word.
+ */
+#define SWORD_BITS \
+	(SEPARATOR_BITS + WORD_BITS)
+
+/*
+ * Maximum number of words to use.
+ */
+#define WORDS_MAX			7
+
+/*
+ * Minimum and maximum number of bits to encode.  With the settings above,
+ * these are 26 and 132, respectively.
+ */
+#define BITS_MIN \
+	(2 * WORD_BITS)
+#define BITS_MAX \
+	(WORD_BITS + WORDS_MAX * (SEPARATOR_BITS + WORD_BITS))
 
 static int read_loop(int fd, unsigned char *buffer, int count)
 {
@@ -52,20 +88,44 @@ char *passwdqc_random(const passwdqc_params_qc_t *params)
 {
 	char output[0x100], *retval;
 	int bits;
-	int use_separators, count, i;
+	int word_count, use_separators, i;
 	unsigned int length, extra;
 	const char *start, *end;
 	int fd;
 	unsigned char bytes[3];
 
 	bits = params->random_bits;
-	if (bits < 26 || bits > 132)
+	if (bits < BITS_MIN || bits > BITS_MAX)
 		return NULL;
 
-	count = 1 + (bits + (16 - 13)) / 17;
-	use_separators = ((bits + 12) / 13 != count);
+/*
+ * Calculate the number of words to use.  The first word is always present
+ * (hence the "1 +" and the "- WORD_BITS").  Each one of the following words,
+ * if any, is prefixed by a separator character, so we use SWORD_BITS when
+ * calculating how many additional words to use.  We divide "bits - WORD_BITS"
+ * by SWORD_BITS with rounding up (hence the addition of "SWORD_BITS - 1").
+ */
+	word_count = 1 + (bits + (SWORD_BITS - 1 - WORD_BITS)) / SWORD_BITS;
 
-	length = count * 7 - 1;
+/*
+ * To determine whether we need to use different separator characters or maybe
+ * not, calculate the number of words we'd need to use if we don't use
+ * different separators.  We calculate it by dividing "bits" by WORD_BITS with
+ * rounding up (hence the addition of "WORD_BITS - 1").  The resulting number
+ * is either the same as or greater than word_count.  Use different separators
+ * only if their use, in the word_count calculation above, has helped reduce
+ * word_count.
+ */
+	use_separators = ((bits + (WORD_BITS - 1)) / WORD_BITS != word_count);
+
+/*
+ * Calculate and check the maximum possible length of a "passphrase" we may
+ * generate for a given word_count.  We add 1 to WORD_LENGTH_MAX to account for
+ * separators (whether different or not).  We subtract 1 because the number of
+ * one-character separators is one less than the number of words.  The check
+ * against sizeof(output) uses ">=" to account for NUL termination.
+ */
+	length = word_count * (WORD_LENGTH_MAX + 1) - 1;
 	if (length >= sizeof(output) || (int)length > params->max)
 		return NULL;
 
@@ -79,11 +139,16 @@ char *passwdqc_random(const passwdqc_params_qc_t *params)
 			return NULL;
 		}
 
+/*
+ * Append a word.  Treating bytes as little-endian, we use bits 0 to 11 for the
+ * word index, and bit 13 for toggling the case of the first character.  Bits
+ * 12, 14, and 15 are left unused.  Bits 16 to 23 are left for the separator.
+ */
 		i = (((int)bytes[1] & 0x0f) << 8) | (int)bytes[0];
 		start = _passwdqc_wordset_4k[i];
-		end = memchr(start, '\0', 6);
+		end = memchr(start, '\0', WORD_LENGTH_MAX);
 		if (!end)
-			end = start + 6;
+			end = start + WORD_LENGTH_MAX;
 		extra = end - start;
 		if (length + extra >= sizeof(output) - 1) {
 			close(fd);
@@ -92,15 +157,24 @@ char *passwdqc_random(const passwdqc_params_qc_t *params)
 		memcpy(&output[length], start, extra);
 		output[length] ^= bytes[1] & 0x20; /* toggle case if bit set */
 		length += extra;
-		bits -= 13;
+		bits -= WORD_BITS;
 
 		if (bits <= 0)
 			break;
 
+/*
+ * Append a separator character.  We use bits 16 to 19.  Bits 20 to 23 are left
+ * unused.
+ *
+ * Special case: we may happen to generate one word less than word_count if the
+ * final separator provides enough bits on its own.  This was not accounted for
+ * in the calculations prior to this loop, but it is here.  With WORD_BITS 13
+ * and SEPARATOR_BITS 4, this happens e.g. for bits values from 66 to 68.
+ */
 		if (use_separators) {
 			i = bytes[2] & 0x0f;
 			output[length++] = SEPARATORS[i];
-			bits -= 4;
+			bits -= SEPARATOR_BITS;
 		} else
 			output[length++] = SEPARATORS[0];
 	} while (bits > 0);
