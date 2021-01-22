fe03231229517623a5bb314af0862ce3f2ce6187
Solar Designer <solar@openwall.com>
2021-01-22 21:32:09 +0100

    passwdqc_filter_lookup(): Hash computation self-test, overwrite memory

--- passwdqc_filter.c
+++ passwdqc_filter.c
@@ -12,6 +12,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#include "passwdqc.h"
 #define PASSWDQC_FILTER_INTERNALS
 #include "passwdqc_filter.h"
 
@@ -60,34 +61,49 @@ int passwdqc_filter_close(passwdqc_filter_t *flt)
 
 static int check(const passwdqc_filter_t *flt, passwdqc_filter_i_t i, passwdqc_filter_f_t f)
 {
+	int retval = -1;
+
 	passwdqc_filter_packed_t p;
 	if (lseek(flt->fd, sizeof(flt->header) + (off_t)i * sizeof(p), SEEK_SET) < 0 ||
 	    read_loop(flt->fd, &p, sizeof(p)) != sizeof(p))
-		return -1;
+		goto out;
 
 	passwdqc_filter_unpacked_t u;
 	unsigned int n = (unsigned int)passwdqc_filter_unpack(&u, &p, NULL);
 	if (n > flt->header.bucket_size)
-		return -1;
+		goto out;
 
 	unsigned int j;
-	for (j = 0; j < n; j++)
-		if (passwdqc_filter_f_eq(u.slots[j], f, flt->header.bucket_size))
-			return 1;
+	for (j = 0; j < n; j++) {
+		if (passwdqc_filter_f_eq(u.slots[j], f, flt->header.bucket_size)) {
+			retval = 1;
+			goto out;
+		}
+	}
 
-	return (n < flt->header.threshold) ? 0 : 2;
+	retval = (n < flt->header.threshold) ? 0 : 2;
+
+out:
+	_passwdqc_memzero(&p, sizeof(p));
+	_passwdqc_memzero(&u, sizeof(u));
+	return retval;
 }
 
 int passwdqc_filter_lookup(const passwdqc_filter_t *flt, const char *plaintext)
 {
+	int retval = 3;
 	passwdqc_filter_hash_t h;
+	passwdqc_filter_f_t ftest;
 
+clean:
 	switch (flt->header.hash_id) {
 	case PASSWDQC_FILTER_HASH_MD4:
 		passwdqc_filter_md4(&h, plaintext);
+		ftest = 0x8c6420f439de2000ULL;
 		break;
 	case PASSWDQC_FILTER_HASH_NTLM_CP1252:
 		passwdqc_filter_ntlm_cp1252(&h, plaintext);
+		ftest = 0x26bd9256ff7e052eULL;
 		break;
 	default:
 		return -1;
@@ -97,13 +113,35 @@ int passwdqc_filter_lookup(const passwdqc_filter_t *flt, const char *plaintext)
 	passwdqc_filter_i_t i = passwdqc_filter_h2i(&h, nbuckets);
 	passwdqc_filter_f_t f = passwdqc_filter_h2f(&h);
 
-	int retval = check(flt, i, f);
-	if (retval <= 1)
+	_passwdqc_memzero(&h, sizeof(h));
+
+/*
+ * The tests of i and f here not only self-test the code, but also prevent the
+ * compiler from moving "return retval;" to before the computation of h, i, and
+ * f, which would leave sensitive data from the real hash computation around.
+ */
+	if (i >= nbuckets)
+		return -1;
+
+	if (retval <= 1) {
+/* Waste two syscalls on overwriting lseek()'s stack and current file offset */
+		i = passwdqc_filter_h2i(&h, nbuckets); /* 0 */
+		if (check(flt, i, f) < 0)
+			return -1;
+		if (f != ftest)
+			return -1;
 		return retval;
+	}
+
+/* At least 1 character to overwrite passwdqc_filter_ntlm_cp1252()'s buffer */
+	plaintext = " 09AZaz\x7e\x7f\x80\x81\x9e\x9f\xa0\xff";
+
+	retval = check(flt, i, f);
+	if (retval <= 1)
+		goto clean;
 
 	retval = check(flt, passwdqc_filter_alti(i, f, nbuckets), f);
-	if (retval <= 1)
-		return retval;
-
-	return 0;
+	if (retval == 2)
+		retval = 0;
+	goto clean;
 }
