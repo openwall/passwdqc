e69cce5dc3911cff54239b0f7b997c8a1dfb761a
Solar Designer <solar@openwall.com>
2025-12-08 00:06:28 +0100

    passwdqc_check: is_based(): Skip pre-check when haystack may be oldpass
    since it's security-sensitive and we do not zeroize the map for speed.

--- passwdqc_check.c
+++ passwdqc_check.c
@@ -290,7 +290,10 @@ static int is_based(const passwdqc_params_qc_t *params,
 	if (params->match_length < 0)	/* misconfigured */
 		return 1;
 
-	{
+	if ((flags & F_MODE) == F_RM) { /* haystack may be sensitive */
+		potential_match_length = haystack_length = (int)strlen(haystack);
+		potential_match_start = 0;
+	} else { /* assume haystack isn't security-sensitive, map it */
 		unsigned char haystack_map[0x100] = {0};
 		for (p = haystack, haystack_length = 0; *p; p++)
 			haystack_map[(unsigned char)*p] = ++haystack_length;
