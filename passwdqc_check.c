8bc85f771204024e34036a26d23ba3329dbba197
Solar Designer <solar@openwall.com>
2026-03-21 15:26:39 +0100

    passwdqc_check: Zeroize more remnants of last tested word
    which could otherwise be hints as to what the rejected password could (not)
    have been.  This is still best effort, not necessarily reliable.

--- passwdqc_check.c
+++ passwdqc_check.c
@@ -290,7 +290,7 @@ static int is_based(const passwdqc_params_qc_t *params,
 	if (unlikely((flags & F_MODE) == F_RM)) { /* haystack may be sensitive */
 		potential_match_length = haystack_length = (int)strlen(haystack);
 		potential_match_start = 0;
-	} else { /* assume haystack isn't security-sensitive, map it */
+	} else { /* assume haystack isn't very security-sensitive, map it */
 		unsigned char haystack_map[0x100] = {0};
 		for (p = haystack, haystack_length = 0; *p; p++)
 			haystack_map[(unsigned char)*p] = ++haystack_length;
@@ -610,6 +610,8 @@ out:
 		free(buf);
 	}
 	_passwdqc_memzero(word, sizeof(word));
+	_passwdqc_memzero(word_unified, sizeof(word_unified));
+	is_based(params, "", "", "", "", F_WORD); /* zeroize haystack_map */
 	return reason;
 }
 
